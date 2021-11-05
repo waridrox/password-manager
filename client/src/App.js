c173b2e7d690450820308c815d6e41b88171e42b
waridrox <mohdwarid4@gmail.com>
2021-11-05 22:18:11 +0530

    show and hide passwords on mouseOver events

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,143 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const notifysuccess = () => toast.success('Password added!', {
+    theme: "colored",
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+  
+  const notifyfailure = () => toast.error('Input fields cannot be empty!', {
+    theme: "colored",
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
 
   const [password, setPassword] = useState('')
   const [website, setWebsite] = useState('')
+  const [passwordList, setPasswordList] = useState([])
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/showpasswords').then((response) => {
+      console.log(response.data)
+      setPasswordList(response.data)
+    })
+  }, []) //[] => only calls when page refreshes
+
 
   const addPassword = () => {
-    Axios.post('http://localhost:3001/addpass', {
-      password: password, 
-      website: website
+    if (password === '' || website === '') {
+      notifyfailure()
+    }
+    else {
+      Axios.post('http://localhost:3001/addpass', {
+        password: password, 
+        website: website
+      })
+      console.log('clearing website input field')
+      setWebsite('')
+      console.log('clearing password input field')
+      setPassword('')
+      notifysuccess()
+    }
+  }
+
+  const decryptPassword = (encryption) => {
+    Axios.post('http://localhost:3001/decryptpassword', {
+      password: encryption.password,
+      iv: encryption.iv,
+      decryptState: false
+    }).then((response) => {
+      console.log(response.data)
+      setPasswordList(passwordList.map((val) => {
+        return (val.id === encryption.id) ? {id: val.id, 
+          password: val.id, 
+          website: response.data, 
+          iv: val.iv, 
+          decryptState: true 
+        } : val 
+      }))
     })
   }
+
   return (
-    <div className="App">
-        <div className="AddPass">
-        {/* <body className="bg-purple-200"> */}
-	<div className="w-full max-w-xs mx-auto mt-8 px-4 font-sans">
-		<form className="bg-white shadow-md rounded-lg px-6 pt-6 pb-8 mb-4">
-			<h3 className="title text-xl mb-8 mx-auto text-center font-bold text-purple-700">Add a new password</h3>
+  <div className="App bg-indigo-200">
+    <div className="AddPass">
+    <ToastContainer
+    position="top-right"
+    autoClose={3000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    />
+	<div className="w-full max-w-xl mx-auto pt-8 pb-8 px-4 font-sans">
+		<form className="bg-white shadow-md rounded-lg px-6 pt-6 pb-8">
+			<h3 className="title text-xl mb-8 mx-auto text-center font-bold text-indigo-600">Password Manager</h3>
 			<div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="website">
+
+      <label className="font-bold block mb-1 mt-6 text-gray-700" for="website">
         Website
       </label>
-				<input type="text" placeholder="Ex. abc.com" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+				<input type="text" placeholder="facebook.com" className="appearance-none border-2 rounded w-full py-3 px-3 leading-tight border-gray-300 bg-gray-100 focus:outline-none focus:border-indigo-700 focus:bg-white text-gray-700 pr-16 font-mono" 
         onChange={(event) => {
           setWebsite(event.target.value)
         }}
         />
-
-        <div className="mb-4 mt-4">
-
-        <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+      <div className="mb-4 mt-4">
+      <label className="font-bold block mb-1 mt-6 text-gray-700" for="password">
         Password
       </label>
-        <input type="password" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 pt-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="password" placeholder="***************" 
-        onChange={(event) => {
-          setPassword(event.target.value)
-        }}
-        />
+      <input type="password" className="appearance-none border-2 rounded w-full py-3 px-3 leading-tight border-gray-300 bg-gray-100 focus:outline-none focus:border-indigo-700 focus:bg-white text-gray-700 pr-16 font-mono" id="password"
+      onChange={(event) => {
+        setPassword(event.target.value)
+      }}
+      />
+      </div>
 
-        </div>
-
-        <div className="w-full items-center">
-					<button onClick={addPassword} className="shadow bg-purple-500 hover:bg-purple-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded mt-4" type="button">
+      <div className="w-full items-center">
+					<button onClick={ () => { addPassword(); }} className="mb-6 shadow bg-indigo-600 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded mt-4" type="button">
 						Add password 
 					</button>
-        </div>
 
-				{/* <div className="flex items-center justify-between">
-					<button className="shadow bg-purple-500 hover:bg-purple-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded mt-4" type="button">
-						Sign Up
-					</button>
-					<a className="inline-block -mb-4 align-baseline font-bold text-sm text-purple-500 hover:text-purple-600 focus:shadow-outline focus:outline-none" href="#">
-          Forgot Password?
-					</a>
-				</div> */}
-			</div>
-		</form>
-	</div>
-{/* </body> */}
+        <div className="passwords items-center flex-wrap justify-center items-baseline">
+          {passwordList.map((val) => {
+            console.log(val)
+            return <div className="w-full mt-3 rounded-lg px-4 py-2 bg-black text-white cursor-pointer hover:bg-yellow-400 hover:text-black" key={val.id} 
+            onMouseOut={() => {
+              Axios.get('http://localhost:3001/showpasswords').then((response) => {
+              console.log(response.data)
+              setPasswordList(response.data)
+            })}} 
+            onMouseOver={() => { 
+              console.log("Decrypting...")
+              decryptPassword({ password: val.password, iv: val.iv, id: val.id })
+            }}> {val.website} </div>
+          })}
+          </div>
+			    </div>
         </div>
-    </div>
+      </form> 
+	  </div>
+  </div>
+</div>
   );
 }
 
