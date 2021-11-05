d9a4702ae180a30edd5e143d38246ef789735208
waridrox <mohdwarid4@gmail.com>
2021-11-05 22:19:04 +0530

    dependencies updated

--- server/index.js
+++ server/index.js
@@ -1,21 +1,14 @@
 const express = require('express')
 const app = express()
-const mysql = require('mysql')
 const PORT = 3001
 const cors = require('cors')
 require('dotenv').config()
 
+const { db } = require('./config/db')
 const {encrypt, decrypt} = require('./EncryptionHandler')
 
 app.use(cors())
 app.use(express.json())
-
-const db = mysql.createConnection({
-    user: 'root', 
-    host: 'localhost',
-    password: 'password',
-    database: 'PasswordManager'
-})
 
 app.post("/addpass", (req, res) => {
     const { password, website } = req.body
